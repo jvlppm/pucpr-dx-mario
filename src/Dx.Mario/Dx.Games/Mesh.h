68a774d85cc612690f55aa4410633d7a6b1e3f76
Joao Vitor P. Moraes <joao.moraes@lactec.org.br>
2015-01-05 14:35:58 -0200

    Fixed unknown build error.

--- src/Dx.Mario/Dx.Games/Mesh.h
+++ src/Dx.Mario/Dx.Games/Mesh.h
@@ -6,7 +6,7 @@
 namespace games {
 	typedef struct Mesh {
 		std::shared_ptr<ID3DXMesh> data;
-		std::vector<D3DMATERIAL9> materials = {};
-		std::vector<std::shared_ptr<IDirect3DTexture9>> textures = {};
+		std::vector<D3DMATERIAL9> materials;
+		std::vector<std::shared_ptr<IDirect3DTexture9>> textures;
 	} Mesh;
 }
